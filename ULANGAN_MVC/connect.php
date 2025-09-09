51702f11e777115f6f96a7f4c7a9ef3e51cd7662
FuntasticMosasaurus <mochamadsoleh97@gmail.com>
2025-09-09 07:07:43 +0700

    Add files via upload
    First Commit

---
+++ ULANGAN_MVC/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'katalogfilm';
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die ("Gagal menghubungkan ke database");
+
+
+
+?>
