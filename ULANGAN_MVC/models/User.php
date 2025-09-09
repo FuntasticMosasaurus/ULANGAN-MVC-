1f777c7521c35a94978f5482b5eaeab0db72abd6
FuntasticMosasaurus <mochamadsoleh97@gmail.com>
2025-09-09 07:24:31 +0700

    Third Commit
    Third Commit

--- ULANGAN_MVC/models/User.php
+++ ULANGAN_MVC/models/User.php
@@ -1,11 +1,29 @@
 <?php
 include 'connect.php';
 
+class User{
+    private $db;
+
+    public function __construct($conn){
+        $this->db = $conn;
+    }
+
+    public function getAllUsers(){
+        $result = $this->db->query("SELECT * FROM katalogfilm");
+        return $result->fetch_all(MSQLY_ASSOC);
+    }
+
+    public function getUserById($id){
+        $stmt = $this->db->prepare("SELECT * FROM katalogfilm WHERE id = ?");
+        $stmt = bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt-get_result();
+        return $result->fetch_assoc();
+    }
 
 
 
-
-
+}
 
 
 
