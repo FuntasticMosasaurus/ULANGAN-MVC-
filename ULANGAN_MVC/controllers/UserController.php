db00300ff7572fbff2f7b03c71776faf4ea545d8
FuntasticMosasaurus <mochamadsoleh97@gmail.com>
2025-09-09 07:34:54 +0700

    Fourth Commit
    Fourth Commit

--- ULANGAN_MVC/controllers/UserController.php
+++ ULANGAN_MVC/controllers/UserController.php
@@ -1,6 +1,27 @@
 <?php
 include 'models/User.php';
 
+class UserController{
+    private $userModel;
+
+    public function __construct($conn){
+        $this->userModel = new User($conn);
+    }
+
+    public function index(){
+        if (isset($_GET['id'])) {
+            $id = (int) $_GET['id'];
+            $user = $this->userModel->getUserById($id);
+            include 'views/user/detail.php';
+        }
+        else{
+            $user = $this->userModel->getAllUsers();
+            include 'views/users.php';
+        }
+
+    }
+}
+
 
 
 
